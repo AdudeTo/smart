718d0e79ab841bdea73f54d181985d95a0c2d9ff
BuildTools <unconfigured@null.spigotmc.org>
2022-03-11 09:22:27 -0800

    add api

---
+++ api.php
@@ -0,0 +1,42 @@
+<?php
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET, POST');
+    header("Access-Control-Allow-Headers: X-Requested-With");
+
+
+    include("db.php");
+    $statLoad = $_GET['data'];
+
+    if($statLoad == 1){
+        $newChildT = [];
+        $getItems = $conn->query("SELECT * FROM data ORDER BY id");
+
+        $i =0;
+        while($row = $getItems->fetch_assoc()) {
+            $myObj =new stdClass();
+            $myObj->state = $row['status'];
+            $myObj->itemId = $row['id'];
+            $myObj->color = $row['color'];
+            $myObj->name = $row['name'];
+            $newChildT[$i] = $myObj;	
+            $i++;	
+        }
+
+        echo json_encode($newChildT, JSON_PRETTY_PRINT);
+    }else if($statLoad == 2){
+        $id = $_GET['id'];
+        $stat = $_GET['stat'];
+
+        $newChildT = [];
+        $myObj =new stdClass();
+        $myObj->id = $id;
+        $myObj->stat = $stat;
+        $newChildT[0] = $myObj;
+
+        $updateItems = $conn->query("UPDATE data  SET status=$stat WHERE id=$id");
+
+        echo json_encode($newChildT, JSON_PRETTY_PRINT);
+
+    }
+?>
